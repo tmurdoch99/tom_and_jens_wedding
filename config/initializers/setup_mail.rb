10a67802557917a34c399ab4bcf215b475aa1265
tom murdoch <tmurdoch_99@yahoo.com>
2013-11-15 15:00:02 +0000

    save arezzo files

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "tom",
-  :user_name            => "tmurdoch_@yahoo.com,
-  :password             => "hollins999",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
